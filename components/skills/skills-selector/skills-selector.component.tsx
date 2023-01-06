40e0c00e7f7124528faf619457dd7290207bd7a4
68550_Rashmi <kulkarnirashmi93@gmail.com>
2023-01-06 12:43:11 +0530

    Code(Search): Search skills components is working.

--- components/skills/skills-selector/skills-selector.component.tsx
+++ components/skills/skills-selector/skills-selector.component.tsx
@@ -3,10 +3,17 @@ import { Skills } from "@prisma/client";
 import { useSelector, useDispatch } from "react-redux";
 import { selectAllSkills } from "../../../store/slices/skills.slice";
 import { addSelectedSkill } from "../../../store/actions";
+import { useState } from "react";
 
 function SkillsSelector() {
   const skills = useSelector(selectAllSkills);
   const dispatch = useDispatch();
+
+  const [searchText, setSearchText] = useState("");
+
+  const filteredSkills = skills.filter((skl) =>
+    skl.name.toLowerCase().includes(searchText.toLowerCase())
+  );
 
   return (
     <Box sx={{ mr: "3vw" }}>
@@ -16,19 +23,22 @@ function SkillsSelector() {
             id="outlined-search"
             label="Search Skills"
             type="search"
-            placeholder="Search Skills"
+            value={searchText}
             fullWidth
+            onChange={(evt) => {
+              setSearchText(evt.target.value);
+            }}
           />
         </Grid>
         <Grid item xs={12} sm={12} md={12}>
-          {skills.map((skill: Skills) => {
+          {filteredSkills.map((skl: Skills) => {
             return (
               <Chip
-                key={skill.id}
-                label={skill.name}
+                key={skl.id}
+                label={skl.name}
                 sx={{ m: "0.2rem" }}
                 onClick={() => {
-                  dispatch(addSelectedSkill(skill.id));
+                  dispatch(addSelectedSkill(skl.id));
                 }}
               />
             );
